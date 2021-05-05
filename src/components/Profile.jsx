a28c5821d3a2a5e8cf7a359c6156e7d333b0350b
rgguriev <rgguryev@gmail.com>
2021-05-05 21:12:10 +0200

    start

---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div className="content-background">
+        <img src="https://picsum.photos/720/200" alt=""/>
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        my posts
+        <div>
+          New posts
+        </div>
+        <div>
+          post 1
+        </div>
+        <div>
+          post 2
+        </div>
+        <div>
+          post 3
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
